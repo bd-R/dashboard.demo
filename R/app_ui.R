9b7e371cce654a720c24145239e8cf094ec86457
RAHUL <rahul.chauhan049@gmail.com>
2019-08-13 13:09:29 +0530

    create dashboard page outline
    Created 5 tabs in shiny dashboard called dataInput, dataSummary, spatial, temporal, taxonomic

--- R/app_ui.R
+++ R/app_ui.R
@@ -1,28 +1,80 @@
 #' @import shiny
+#' @import shinydashboard
+#' @import dashboardthemes
+
 app_ui <- function() {
-  tagList(
-    # Leave this function for adding external resources
-    golem_add_external_resources(),
-    # List the first level UI elements here 
-    fluidPage(
-      h1("bdvisDashboard")
-    )
-  )
+  dashboardPage(
+    skin = "green",
+    dashboardHeader(title = "bdvisashboard"),
+    
+    
+    #----------------------SideBar Start-------------------------------------
+    dashboardSidebar(
+      sidebarMenu(
+        id = "sideBar",
+        menuItem(
+          "Data Input",
+          tabName = "dataInputTab",
+          icon = icon("database")
+        ),
+        
+        menuItem(
+          "Data Summary",
+          tabName = "dataSummary",
+          icon = icon("database")
+        ),
+        
+        menuItem(
+          "Spatial Visualization",
+          tabName = "spatialTab",
+          icon = icon("map-marked")
+        ),
+        
+        menuItem(
+          "Taxonomic Visualization",
+          tabName = "taxonomicTab",
+          icon = icon("connectdevelop")
+        ),
+        
+        menuItem(
+          "Temporal Visualization",
+          tabName = "temporalTab",
+          icon = icon("clock")
+        )
+      )#Sidebar menu ends here
+    ),
+    #sidebar Dashboard ends here
+    #----------------------SideBar End-------------------------------------
+    
+    
+    
+    #----------------------Body Start-------------------------------------
+    dashboardBody(
+      shinyDashboardThemes(theme = "grey_dark"),
+      golem_add_external_resources(),
+      tabItems(
+        
+        
+      )
+    )#Dashboard Body ends here
+    
+    #-------------------------Body Ends Here-------------------------------
+    
+  )#End of dashboard page
 }
 
 #' @import shiny
-golem_add_external_resources <- function(){
+golem_add_external_resources <- function() {
+  addResourcePath('www', system.file('app/www', package = 'bdvisShiny'))
   
-  addResourcePath(
-    'www', system.file('app/www', package = 'bdvisDashboard')
-  )
- 
   tags$head(
     golem::activate_js(),
     golem::favicon()
+    
+    
     # Add here all the external resources
     # If you have a custom.css in the inst/app/www
     # Or for example, you can add shinyalert::useShinyalert() here
-    #tags$link(rel="stylesheet", type="text/css", href="www/custom.css")
+    #tags$link(rel = "stylesheet", type = "text/css", href = "www/custom.css")
   )
 }
