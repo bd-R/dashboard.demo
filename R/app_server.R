c8f113c02d850086576f61216ff989d5c83604ab
RAHUL <rahul.chauhan049@gmail.com>
2019-08-13 14:28:21 +0530

    added 5 sidebar tabs using shinydashboard

--- R/app_server.R
+++ R/app_server.R
@@ -1,4 +1,6 @@
 #' @import shiny
-app_server <- function(input, output,session) {
-  # List the first level callModules here
+options(shiny.maxRequestSize = 5000 * 1024 ^ 2)
+
+app_server <- function(input, output, session) {
+   
 }
