58429b7636fa6e7c56fe33dc81e2a8c410bea4ca
tom-gu <tomer.gu@gmail.com>
2019-11-17 14:53:08 +0200

    setting launch.browser = TRUE

--- R/run_app.R
+++ R/run_app.R
@@ -5,7 +5,7 @@
 #' @importFrom golem with_golem_options
 run_app <- function(...) {
   with_golem_options(
-    app = shinyApp(ui = app_ui, server = app_server), 
+    app = shinyApp(ui = app_ui, server = app_server, options = c(launch.browser = TRUE)), 
     golem_opts = list(...)
   )
 }
