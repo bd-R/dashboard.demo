b1af25bb44e1cfb9f9c471ef3ea5a22238e618ba
tom-gu <tomer.gu@gmail.com>
2019-11-17 18:39:09 +0200

    removing unnecessary dependencies

--- R/app_ui.R
+++ R/app_ui.R
@@ -1,6 +1,6 @@
-#' @import shiny shinydashboard leaflet dplyr dashboardthemes ggpubr plotly
-#' @import data.tree flexdashboard bdvis ggplot2 tidyr treemap
-#' @import formattable summarytools leaflet.extras sp bdutilities.app
+#' @import shiny shinydashboard leaflet dplyr dashboardthemes plotly
+#' @import flexdashboard DT
+#' @import formattable leaflet.extras sp bdutilities.app
 
 app_ui <- function() {
   dashboardPage(
