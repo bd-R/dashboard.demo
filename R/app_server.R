1646a5ca4f600c296ff6b191c0372ca37eee9dd4
tom-gu <tomer.gu@gmail.com>
2019-11-17 11:00:46 +0200

    enlarging the map zize to 400px

--- R/app_server.R
+++ R/app_server.R
@@ -1,8 +1,10 @@
 #' @import shiny
-options(shiny.maxRequestSize = 5000 * 1024 ^ 2)
+
 
 app_server <- function(input, output, session) {
-
+  
+  options(shiny.maxRequestSize = 5000 * 1024 ^ 2)
+  
   inputDataset <-
     callModule(
       bdutilities.app::mod_add_data_server,
